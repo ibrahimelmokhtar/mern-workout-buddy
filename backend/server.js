c0489540db2064ab5f3d1e739bdcb19884d743ad
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-10-24 21:44:23 +0200

    Backend: connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 // Configure port number
 dotenv.config();
@@ -10,9 +11,25 @@ const PORT = process.env.SERVER_PORT || 8000;
 const app = express();
 
 // Used middle-wares
-app.use(morgan('tiny'));
+app.use([morgan('tiny'), express.json()]);
 
-// Start server
-app.listen(PORT, () => {
-	console.log(`Listening on http://localhost:${PORT}`);
+// Test server/database connection
+app.get('/api/workouts', (req, res) => {
+	res.status(200).json({
+		message: 'Data received successfully',
+	});
 });
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		// Start receiving requests on specific port
+		app.listen(PORT, () => {
+			console.log(
+				`Connected to database && Listening on http://localhost:${PORT}`
+			);
+		});
+	})
+	.catch((error) => {
+		console.error(error);
+	});
