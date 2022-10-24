7a775346f8fbefaec2ad56b8cdd1c46467642038
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-10-24 22:30:28 +0200

    Backend: modify workout controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,33 +1,76 @@
+import Workout from '../models/workoutModel.js';
+
 /* ********************************************************** */
 /* **********************  CREATE  ************************** */
 /* ********************************************************** */
-export const createWorkout = (req, res) => {
-	res.status(200).json({ message: 'Workout is added successfully' });
+export const createWorkout = async (req, res) => {
+	const { title, reps, load } = req.body;
+	try {
+		const workout = await Workout.create({ title, reps, load });
+		res.status(200).json(workout);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
 
 /* ********************************************************** */
 /* ***********************  READ  *************************** */
 /* ********************************************************** */
-export const getAllWorkouts = (req, res) => {
-	res.status(200).json({ message: 'All workouts are sent successfully' });
+export const getAllWorkouts = async (_, res) => {
+	try {
+		const workouts = await Workout.find({}).sort({ createdAt: -1 });
+		res.status(200).json(workouts);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
-export const getWorkout = (req, res) => {
-	res.status(200).json({ message: 'Workout is sent successfully' });
+
+export const getWorkout = async (req, res) => {
+	const { id } = req.body;
+	try {
+		const workout = await Workout.findById(id);
+		if (!workout) return res.status(404).json({ error: 'No such workout!' });
+
+		res.status(200).json(workout);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
 
 /* ********************************************************** */
 /* **********************  UPDATE  ************************** */
 /* ********************************************************** */
-export const updateWorkout = (req, res) => {
-	res.status(200).json({ message: 'Workout is updated successfully' });
+export const updateWorkout = async (req, res) => {
+	const { id, updatedWorkout } = req.body;
+	try {
+		const workout = await Workout.findByIdAndUpdate(id, updatedWorkout);
+		if (!workout) return res.status(404).json({ error: 'No such workout' });
+
+		res.status(200).json(workout);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
 
 /* ********************************************************** */
 /* **********************  DELETE  ************************** */
 /* ********************************************************** */
-export const deleteAllWorkouts = (req, res) => {
-	res.status(200).json({ message: 'All workouts are deleted successfully' });
+export const deleteAllWorkouts = async (_, res) => {
+	try {
+		const workouts = await Workout.deleteMany({});
+		res.status(200).json(workouts);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
-export const deleteWorkout = (req, res) => {
-	res.status(200).json({ message: 'Workout is deleted successfully' });
+
+export const deleteWorkout = async (req, res) => {
+	const { id } = req.body;
+	try {
+		const workout = await Workout.findByIdAndDelete(id);
+		if (!workout) return res.status(404).json({ error: 'No such workout' });
+		res.status(200).json(workout);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
